e5df62ef5589c083e572a362f707de6b510da264
A3BROSLOW <a3broslow@A3BROSLOWs-MacBook-Pro.local>
2024-09-17 16:02:50 +0100

    Calendar well deployed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import TaskBoard from './TaskBoard';
+import CalendarView from './CalendarView';
 
 function App() {
     return (
         <div className="App">
             <h1>Dashboard Mock-up</h1>
             <TaskBoard />
+            <CalendarView />
         </div>
     );
 }
